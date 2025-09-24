8f09bf9e86ef0e02eeb71b248fecdff5bca0ebef
Dereit <106438546+Dereit@users.noreply.github.com>
2025-09-24 09:41:13 +0500

    Task1

---
+++ Tyuiu.GroshevID.Sprint1.Task1.V7.Lib/DataService.cs
@@ -0,0 +1,12 @@
+﻿using tyuiu.cources.programming.interfaces.Sprint1;
+
+namespace Tyuiu.GroshevID.Sprint1.Task1.V7.Lib
+{
+    public class DataService : ISprint1Task1V7
+    {
+        public double Calculate(double x, double y)
+        {
+            return (5 * x) / (y + x);
+        }
+    }
+}
---
+++ Tyuiu.GroshevID.Sprint1.Task1.V7.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.GroshevID.Sprint1.Task1.V7.Lib;
+
+namespace Tyuiu.GroshevID.Sprint1.Task1.V7.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidExpression()
+        {
+            DataService ds = new DataService();
+            double x = 1.0;
+            double y = 3.0;
+            var res = ds.Calculate(x, y);
+            Assert.AreEqual(1.25, res);
+        }
+    }
+}
